16c7585adb3d55b7f850da9d5a6bf1751400adb1
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-08 08:53:35 +0000

    added PriceRunner compariosn site

--- controllers/compare.js
+++ controllers/compare.js
@@ -1,18 +1,28 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const hidden = require("puppeteer-extra-plugin-stealth");
+const { executablePath } = require("puppeteer");
 
 const scrapeWeb = async (item) => {
+	// Stealth
+	puppeteer.use(hidden());
 	// Regex
 	const updatedItem = item.split(" ").join("+");
 	// Open browser
-	const browser = await puppeteer.launch({ headless: true });
+	const browser = await puppeteer.launch({
+		args: ["--no-sandbox"],
+		headless: false,
+		ignoreHTTPSErrors: true,
+		executablePath: executablePath(),
+	});
 
 	// Opens new tab in browser
 	const page = await browser.newPage();
-	await page.setUserAgent(
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-	);
+	// await page.setUserAgent(
+	// 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+	// );
 	// Enter link in tab
 	await page.goto(`https://www.idealo.co.uk/mscat.html?q=${updatedItem}`);
+
 	/* Getting Multiple Elements */
 	const elements = await page.evaluate(() => {
 		const allElements = document.querySelectorAll(".offerList-itemWrapper");
@@ -92,6 +102,95 @@ const topDeals = async () => {
 	return elements;
 };
 
+const priceRunner = async (item) => {
+	// Stealth
+	puppeteer.use(hidden());
+	// Regex
+	const updatedItem = item.split(" ").join("+");
+	// Open browser
+	const browser = await puppeteer.launch({
+		args: ["--no-sandbox"],
+		headless: false,
+		ignoreHTTPSErrors: true,
+		executablePath: executablePath(),
+	});
+
+	// Opens new tab in browser
+	const page = await browser.newPage();
+	// await page.setUserAgent(
+	// 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+	// );
+	// Enter link in tab
+	await page.goto(
+		`https://www.pricerunner.com/results?q=${updatedItem}&suggestionsActive=true&suggestionClicked=false&suggestionReverted=false`
+	);
+
+	await page.click(".KYuPdOZh2w.pr-c9kuyn");
+
+	/* Getting Multiple Elements */
+	const elements = await page.evaluate(() => {
+		let array = [];
+		let links = [];
+		let images = [];
+		let ratings = [];
+		let descriptions = [];
+		let watchers = [];
+
+		const allElements = document.querySelectorAll(".k6oEmfY83J.pr-6r3upd");
+		allElements.forEach((item, i) => {
+			// Title
+			const title = item.querySelector(".pr-7iigu3");
+			// Price
+			const price = item.querySelector(".pr-be5x0o");
+
+			array.push({
+				title: title.innerText,
+				price: price.innerText,
+			});
+		});
+
+		const allLinks = document.querySelectorAll(".k6oEmfY83J.pr-6r3upd a");
+		allLinks.forEach((item, i) => {
+			links.push({ link: item.href });
+		});
+
+		const allImgs = document.querySelectorAll(".xUAmIg3IAt.pr-1jnu7fb");
+		allImgs.forEach((item, i) => {
+			images.push({ img: item.src });
+		});
+
+		const allRatings = document.querySelectorAll(".pr-1ob9nd8");
+		allRatings.forEach((item, i) => {
+			ratings.push({ ratings: item.innerText });
+		});
+
+		const allDesc = document.querySelectorAll(".pr-13b83wt");
+		allDesc.forEach((item, i) => {
+			descriptions.push({ ratings: item.innerText });
+		});
+
+		const allWatchers = document.querySelectorAll(".pr-13vcnfy");
+		allWatchers.forEach((item, i) => {
+			watchers.push({ ratings: item.innerText });
+		});
+
+		const AllData = array.map((item, i) => {
+			return {
+				item,
+				...links[i],
+				...images[i],
+				...ratings[i],
+				...descriptions[i],
+				...watchers[i],
+			};
+		});
+
+		return AllData;
+	});
+
+	return elements;
+};
+
 const comparePrice = async (req, res) => {
 	const { item } = req.params;
 	try {
@@ -111,4 +210,14 @@ const getTopDeals = async (req, res) => {
 	}
 };
 
-module.exports = { comparePrice, getTopDeals };
+const comparePriceAlt = async (req, res) => {
+	const { item } = req.params;
+
+	try {
+		const data = await priceRunner(item);
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+module.exports = { comparePrice, getTopDeals, comparePriceAlt };
--- routes/compare.js
+++ routes/compare.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { comparePrice, getTopDeals } = require("../controllers/compare");
+const {
+	comparePrice,
+	getTopDeals,
+	comparePriceAlt,
+} = require("../controllers/compare");
 
 router.route("/:item").get(comparePrice);
 router.route("/items/top-deals").get(getTopDeals);
+router.route("/alt/:item").get(comparePriceAlt);
 
 module.exports = router;
